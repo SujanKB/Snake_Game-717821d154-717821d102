a3e81d327e4a8da7c79c7530524a7f0675df0533
SujanKB <137860376+SujanKB@users.noreply.github.com>
2024-03-14 09:34:13 +0530

    Delete 717821L363 & 717821L313 directory

--- 717821L363 & 717821L313/Movie Purchasing System/com/kce/dao/MovieDAO.java
+++
@@ -1,9 +0,0 @@
-package com.kce.dao;
-import com.kce.bean.*;
-public interface MovieDAO {
-    void createMovie(Movie movie);
-    Movie readMovie(int movieId);
-    void updateMovie(Movie movie);
-    void deleteMovie(int movieId);
-    void updateAvailability(int movieId, boolean availability);
-}

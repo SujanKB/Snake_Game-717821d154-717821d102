3c8416298de2d50289dcb9fa8fa1509055713ff9
SujanKB <137860376+SujanKB@users.noreply.github.com>
2024-03-14 09:36:53 +0530

    Delete 717821T159 &717821T141 directory

--- 717821T159 &717821T141/Hotel_Book/src/com/kce/hotel/util/OverbookingException.java
+++
@@ -1,7 +0,0 @@
-package com.kce.hotel.util;
-public class OverbookingException extends Exception {
-	private static final long serialVersionUID = 1L;
-	public OverbookingException(String message){
-        super(message);
-    }
-}
--- 717821T159 &717821T141/Hotel_Book/src/com/kce/hotel/util/RoomUnavailableException.java
+++
@@ -1,8 +0,0 @@
-package com.kce.hotel.util;
-public class RoomUnavailableException extends Exception{
-	private static final long serialVersionUID = 1L;
-    public RoomUnavailableException(String message) {
-	        super(message);
-	    }
-	}
-

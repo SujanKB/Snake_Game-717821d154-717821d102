d2bfec7d2a18b837bb1c7d4ae57ab55f0f25a304
SujanKB <137860376+SujanKB@users.noreply.github.com>
2024-03-14 10:13:23 +0530

    Delete flight directory

--- flight/src/com/kce/service/customer.java
+++
@@ -1,9 +0,0 @@
-package com.kce.service;
-import com.kce.bean.BookingDetails;
-
-public class customer extends BookingDetails {
-	public Customer(String name, int phNo, int flightNo, int noOfSeats) {
-		super(name, phNo, flightNo, noOfSeats);
-	}
-
-}

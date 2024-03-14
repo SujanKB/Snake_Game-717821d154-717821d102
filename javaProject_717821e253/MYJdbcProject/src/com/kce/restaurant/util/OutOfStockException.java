07b70fc7a639352c18098a8130b18847edd5b483
SujanKB <137860376+SujanKB@users.noreply.github.com>
2024-03-14 10:13:43 +0530

    Delete javaProject_717821e253 directory

--- javaProject_717821e253/MYJdbcProject/src/com/kce/restaurant/util/OutOfStockException.java
+++
@@ -1,9 +0,0 @@
-package com.kce.restaurant.util;
-
-@SuppressWarnings("serial")
-public class OutOfStockException extends Exception
-{
- public OutOfStockException(String str) {
-	 super(str);
- }
-}

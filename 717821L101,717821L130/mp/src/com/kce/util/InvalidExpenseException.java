eccb513561a4a6036f224142f8ef3f94ac14d4b6
SujanKB <137860376+SujanKB@users.noreply.github.com>
2024-03-14 09:28:45 +0530

    Delete 717821L101,717821L130 directory

--- 717821L101,717821L130/mp/src/com/kce/util/InvalidExpenseException.java
+++
@@ -1,9 +0,0 @@
-package com.kce.util;
-
-// User defined Exception Class
-public class InvalidExpenseException extends Exception {
-	private static final long serialVersionUID = 1L;
-	public InvalidExpenseException(String message) {
-        super(message);
-    }
-}

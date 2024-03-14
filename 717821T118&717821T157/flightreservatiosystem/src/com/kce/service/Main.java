b6d8fec89eb15f500c04e4bb4d110f16a1ae6f54
SujanKB <137860376+SujanKB@users.noreply.github.com>
2024-03-14 09:35:48 +0530

    Delete 717821T118&717821T157 directory

--- 717821T118&717821T157/flightreservatiosystem/src/com/kce/service/Main.java
+++
@@ -1,9 +0,0 @@
-package com.kce.service;
-
-public class Main {
-	public static void main(String[] args) throws ClassNotFoundException {
-		ConsoleMenu cm = new ConsoleMenu();
-		cm.displayMenu();
-	}
-
-}

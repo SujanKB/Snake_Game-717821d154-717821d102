e6f26088fbf267f0c558df05b4cf6cfad730d219
SujanKB <137860376+SujanKB@users.noreply.github.com>
2024-03-14 09:34:21 +0530

    Delete 717821P121 & 717821P151 directory

--- 717821P121 & 717821P151/hotel_management_system/src/com/kce/hotel/util/HotelException.java
+++
@@ -1,7 +0,0 @@
-package com.kce.hotel.util;
-
-public class HotelException extends Exception{
-    public String toString(){
-        return "The Hotel Id is unavailable! \n Please enter another Id.";
-    }
-}
